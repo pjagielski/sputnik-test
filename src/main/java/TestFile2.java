7cf3e8712afc493db4c07c1167c8c78b7737a9c1
Piotr Jagielski <pjg@touk.pl>
2015-10-02 14:37:00 +0200

    test

---
+++ src/main/java/TestFile2.java
@@ -0,0 +1,15 @@
+class TestFile2 {
+    public String foo() {
+        return "bar";
+    }
+
+    private static void incorrectAssignmentInIfCondition() {
+        boolean value = false;
+        if (value = true) {
+            //do Something
+            value = false;
+        } else {
+            //else Do Something
+        }
+    }
+}
