85b41504891cf192383f1ea499fb86487a2fc21e
Marcin Smialek <msm@touk.pl>
2016-01-14 18:40:59 +0100

    correct Sputnik violations

--- src/main/java/TestFile.java
+++ src/main/java/TestFile.java
@@ -3,33 +3,33 @@ class TestFile {
         return "bar";
     }
 
-    private static void incorrectAssignmentInIfCondition() {
-        boolean value = false;
-        if (value = true) {
-            //do Something
-        } else {
-            //else Do Something
-        }
-    }
-
-    private static void incorrectComparingToItself() {
-        int x = 342;
-        if (x == x) {
-            // whatever
-        }
-    }
-
-    private static void anotherVariationOfIncorrectComparingToItself() {
-        int x = 567;
-        if (x <= x) {
-            // whatever
-        }
-    }
-
-    private static void onMoreVariationOfIncorrectComparingToItself() {
-        int x = 23422;
-        if (x > x) {
-            // whatever
-        }
-    }
+//    private static void incorrectAssignmentInIfCondition() {
+//        boolean value = false;
+//        if (value = true) {
+//            //do Something
+//        } else {
+//            //else Do Something
+//        }
+//    }
+//
+//    private static void incorrectComparingToItself() {
+//        int x = 342;
+//        if (x == x) {
+//            // whatever
+//        }
+//    }
+//
+//    private static void anotherVariationOfIncorrectComparingToItself() {
+//        int x = 567;
+//        if (x <= x) {
+//            // whatever
+//        }
+//    }
+//
+//    private static void onMoreVariationOfIncorrectComparingToItself() {
+//        int x = 23422;
+//        if (x > x) {
+//            // whatever
+//        }
+//    }
 }
